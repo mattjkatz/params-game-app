44d47afd33fb2515416553285b1e030b8cbbbeaa
mattjkatz <matthewkatz7@gmail.com>
2022-03-09 19:03:26 -0600

    define the first method to return your name in caps and a secret message if your name starts with 'a'

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,7 +1,12 @@
 class ParamsController < ApplicationController
 
   def params_game
-    render json: "We're shmoovin'"
+    input_name = params["name"].upcase
+    if input_name.split("")[0] == "A"
+      render json: "#{input_name}. Hey, your name starts with the first letter of the alphabet!"
+    else
+      render json: "#{input_name}"
+    end
   end
 
 end
