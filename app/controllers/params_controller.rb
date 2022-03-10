bd2892ee5e4d1919323de9ea52cb4e32dc82b32a
mattjkatz <matthewkatz7@gmail.com>
2022-03-09 18:50:00 -0600

    establish a first route and corresponding method

---
+++ app/controllers/params_controller.rb
@@ -0,0 +1,7 @@
+class ParamsController < ApplicationController
+
+  def params_game
+    render json: "We're shmoovin'"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get "/params_game" => "params#params_game"
+  get "/params_game", controller: "params", action: "params_game"
 end
