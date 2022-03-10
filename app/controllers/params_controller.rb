195f39386197bb8cb5b34190f19ff6f35efb61da
mattjkatz <matthewkatz7@gmail.com>
2022-03-09 20:20:04 -0600

    add url segment params for name_game and number_game

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,11 +1,22 @@
 class ParamsController < ApplicationController
 
-  def params_game
+  def name_game
     input_name = params["name"].upcase
-    if input_name.split("")[0] == "A"
+    if input_name.starts_with?("A")
       render json: "#{input_name}. Hey, your name starts with the first letter of the alphabet!"
     else
       render json: "#{input_name}"
+    end
+  end
+
+  def number_game
+    winning_number = 73
+    if params["number"].to_i == winning_number
+      render json: "You win!!!"
+    elsif params["number"].to_i < winning_number
+      render json: "Too low..."
+    elsif params["number"].to_i > winning_number
+      render json: "Too high!"
     end
   end
 
